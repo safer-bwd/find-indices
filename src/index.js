65a6c75ccb2e938e44b09ed5359539b3db7231af
safer <asafron2@ford.com>
2019-09-04 18:58:35 +0300

    docs: add api docs

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 import findIndex from 'lodash.findindex';
 
+/**
+ * @name find-indices
+ * @param {Array} array The array to search
+ * @param {Function|Array|Object|string} predicate The function invoked per iteration
+ * @param {number} [fromIndex=0] The index to search from
+ * @param {number} [limit=-1] The maximum number of indexes to search
+ * @return {Array<number>} The array of found indexes
+ * @example
+ * import findIndexes from 'find-indices';
+ * const coll = [{ v: 0 }, { v: 2 }, { v: 0 }, { v: 3 }, { v: 3 }];
+ * findIndexes(coll, ({ v }) => v > 1); // [1, 3, 4]
+ * findIndexes(coll, { v: 1 }); // [0, 2]
+ * findIndexes(coll, ['v', 3]); // [3, 4]
+ * findIndexes(coll, 'v'); // [1, 3, 4]
+ * */
 export default (array, predicate, fromIndex = 0, limit = -1) => {
   if (array.length === 0) {
     return [];
