8f782f5e5441543cbedba48d80c43e6ef2138c44
safer <asafron2@ford.com>
2019-09-04 15:40:12 +0300

    feature: implementation 

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
+import findIndex from 'lodash.findindex';
+
 export default (array, predicate, fromIndex = 0, limit = -1) => {
-  return [];
+  if (array.length === 0) {
+    return [];
+  }
+
+  if (limit === 0) {
+    return [];
+  }
+
+  const search = (current, indexes) => {
+    const found = findIndex(array, predicate, current);
+    if (found === -1) {
+      return indexes;
+    }
+
+    indexes.push(found);
+    if (limit > 0 && indexes.length === limit) {
+      return indexes;
+    }
+
+    return search(found + 1, indexes)
+  }
+
+  return search(fromIndex, []);
 };
